78bd478bf20ee018b57ac08dabccebdad9029dd8
Shane Davis <shanedavis@Shanes-MacBook-Pro.local>
2019-12-04 15:52:00 -0600

    created own error handleing

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -45,6 +45,7 @@ namespace DatingApp.API.Controllers
     [HttpPost("login")]
     public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
     {
+
       var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
       if (userFromRepo == null)
@@ -72,7 +73,8 @@ namespace DatingApp.API.Controllers
 
       var token = tokenHandler.CreateToken(tokenDescriptor);
 
-      return Ok(new {
+      return Ok(new
+      {
         token = tokenHandler.WriteToken(token)
       });
     }
